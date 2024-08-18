9ea8ca5014c7865ca95a16190646120ee3fa8106
Gege <28988586+gegelendvay@users.noreply.github.com>
2024-08-18 13:18:33 +0200

    Initial commit

---
+++ client.lua
@@ -0,0 +1,33 @@
+local isFlipped = false
+
+local function Notification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    SetNotificationMessage("CHAR_CALL911", "CHAR_CALL911", true, 1, "Warning!", "~r~Vehicle Accident")
+    DrawNotification(false, false)
+end
+
+CreateThread(function()
+    while true do
+        Wait(100)
+        local player = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(player, false)
+        if vehicle ~= 0 then
+            if IsEntityUpsidedown(vehicle) and not isFlipped then
+                isFlipped = true
+                SetVehicleEngineHealth(vehicle, 300)
+                SetVehicleUndriveable(vehicle, true)
+                Notification("Your vehicle has flipped over!\nThe engine is damaged.")
+                ShakeGameplayCam("SMALL_EXPLOSION_SHAKE", 1.0)
+                Wait(2000)
+                StopGameplayCamShaking(false)
+            end
+
+            if not IsEntityUpsidedown(vehicle) and isFlipped then
+                isFlipped = false
+            end
+        else
+            isFlipped = false
+        end
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Gege'
+version '1.0.0'
+description 'Vehicle rollover detection'
+
+client_script 'client.lua'
